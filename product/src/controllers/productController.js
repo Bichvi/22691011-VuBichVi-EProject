62a80511473a7bf0b259ac448c53f4efc76156c0
BichVi <vubi8266@gmail.com>
2025-11-07 22:57:34 +0700

    ci/cd

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -107,10 +107,15 @@ class ProductController {
 
 
 
-  async getP_ID(req, res){
-    const p = await Product.findById(req.params.id);
-    res.status(200).json(p);
-  }
+  // async getP_ID(req, res){
+  //   const p = await Product.findById(req.params.id);
+  //   res.status(200).json(p);
+  // }
+async getPid(req, res){
+  const p = await Product.findById(req.params.id);
+  res.status(200).json(p);
+}
+
 
   async getProducts(req, res, next) {
     try {
